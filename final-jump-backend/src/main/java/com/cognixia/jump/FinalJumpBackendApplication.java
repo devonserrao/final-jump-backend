066e03b74997b166ed0a49358660b7b743673cc9
devonserrao <devonserrao101@gmail.com>
2021-09-15 15:30:05 -0500

    Adding to github

---
+++ final-jump-backend/src/main/java/com/cognixia/jump/FinalJumpBackendApplication.java
@@ -0,0 +1,13 @@
+package com.cognixia.jump;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinalJumpBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FinalJumpBackendApplication.class, args);
+	}
+
+}
